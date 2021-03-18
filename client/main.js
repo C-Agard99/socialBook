323d9b49b0c00aa9bc9956c5bab7a51b756dc235
ChanelAgard <chanel.agard99@gmail.com>
2021-03-18 15:42:31 -0400

    Updated database and output

--- client/main.js
+++ client/main.js
@@ -4,22 +4,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './main.html';
-import './myAccounts';
+import './myAccounts.html';
+import './addProfile.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+import '../lib/collection.js';
+import './myAccounts.js';
+import './addProfile.js';
