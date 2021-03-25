bb3b5d25bfe15f865eec049bd8a1b1c294cc634b
ChanelAgard <chanel.agard99@gmail.com>
2021-03-25 16:17:15 -0400

    updated user inferface

--- client/addProfile.js
+++ client/addProfile.js
@@ -1,16 +1,22 @@
 Template.addProfile.events({
     'click .js-save'(event){
+        //grabs data from html page
+        let profPic = $('#profPic').val();
         let profFirst = $('#firstName').val();
         let profLast = $('#lastName').val();
         let profAge = $('#age').val();
-        //console.log(profFirst, profLast, profAge);
+        //save data into collection
         profilesdb.insert({
+            "pPic":profPic,
             "pFirst":profFirst,
             "pLast":profLast,
             "pAge":profAge
         });
+        //clear input boxes
+        $('#profPic').val("");
         $('#firstName').val("");
         $('#lastName').val("");
         $('#age').val("");
+        $('#addModal').modal('hide');
     }
 });
