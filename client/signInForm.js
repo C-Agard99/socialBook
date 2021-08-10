81a989f9342fadfc5ac97eb66aad9dced9be0339
ChanelAgard <chanel.agard99@gmail.com>
2021-08-10 11:46:20 -0400

    customized user accounts interface

--- client/signInForm.js
+++ client/signInForm.js
@@ -2,4 +2,10 @@ Template.signInForm.events({
     'click .js-logout'(){
         AccountsTemplates.logout();
     }  
-})
+});
+
+Template.signInForm.helpers({
+    username(){
+        return Meteor.user().username//emails[0].address;
+    }
+});
