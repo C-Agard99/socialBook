3b08b152d7851605b824eacbdd94d14838cfae70
ChanelAgard <chanel.agard99@gmail.com>
2021-08-11 20:17:20 -0400

    updated logic to show new profiles

--- client/addProfile.js
+++ client/addProfile.js
@@ -38,7 +38,8 @@ Template.addProfile.events({
                 "pLast":profLast,
                 "pAge":profAge,
                 "pGen":profGen,
-                "pOwn":Meteor.userId()
+                "pOwn":Meteor.userId(),
+                "createdOn": new Date().getTime()
             });
             //clear input boxes
             document.getElementById("preImg").src = "blankprofile.png";
